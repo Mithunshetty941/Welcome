f96fcacdea23761b98c435bf461d213c98159ca3
Mithunshetty941 <63301068+Mithunshetty941@users.noreply.github.com>
2020-05-11 20:03:20 +0530

    Added CallMe Method

--- Hello.java
+++ Hello.java
@@ -1,6 +1,12 @@
 class Hello{
+  public void callMe(String name)
+  {
+    System.out.println("Hello"+name);
+  }
 public static void main(String[] args)
 {
 System.out.println("Welcome to New Era !");
+  Hello obj=new Hello();
+  obj.callMe("Raj");
 }
 }
